bb99a09d5e495570bfd0fb9c79c04a63307a32d1
Lysender <lysender@darkstar-win>
2024-09-24 16:22:57 +0800

    fix: reconnect when connection is lost

--- src/client.rs
+++ src/client.rs
@@ -1,13 +1,27 @@
 use std::{
     io::{BufRead, BufReader},
     net::TcpStream,
+    thread,
+    time::Duration,
 };
 
 use tracing::{error, info};
 
-use crate::config::Config;
+use crate::{config::Config, Result};
 
 pub fn start_client(config: &Config) {
+    loop {
+        let conn = connect(&config);
+        if let Err(e) = conn {
+            error!("Error connecting to the server: {}", e);
+            info!("Reconnecting in 10 seconds...");
+
+            thread::sleep(Duration::from_secs(10));
+        }
+    }
+}
+
+fn connect(config: &Config) -> Result<()> {
     let stream = TcpStream::connect(&config.server_address).unwrap();
     info!("Connected to the server: {}", config.server_address);
 
@@ -20,8 +34,10 @@ pub fn start_client(config: &Config) {
             }
             Err(e) => {
                 error!("Error reading message: {}", e);
-                break;
+                return Err("Error reading message".into());
             }
         }
     }
+
+    Ok(())
 }
